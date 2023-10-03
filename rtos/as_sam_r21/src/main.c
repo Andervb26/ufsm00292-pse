bb7653e8cebcf2e72c45c08715551a94f8e577cd
Anderson Venzke Backes <anderson.backes@acad.ufsm.br>
2023-10-03 16:49:31 -0300

    Exercício 1 - sistemas operacionais multitarefas (threads)

--- rtos/as_sam_r21/src/main.c
+++ rtos/as_sam_r21/src/main.c
@@ -40,6 +40,7 @@ void tarefa_5(void);
 void tarefa_6(void);
 void tarefa_7(void);
 void tarefa_8(void);
+void tarefa_9(void);
 
 /*
  * Configuracao dos tamanhos das pilhas
@@ -52,6 +53,7 @@ void tarefa_8(void);
 #define TAM_PILHA_6			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_7			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_8			(TAM_MINIMO_PILHA + 24)
+#define TAM_PILHA_9			(TAM_MINIMO_PILHA + 24)
 #define TAM_PILHA_OCIOSA	(TAM_MINIMO_PILHA + 24)
 
 /*
@@ -65,6 +67,7 @@ uint32_t PILHA_TAREFA_5[TAM_PILHA_5];
 uint32_t PILHA_TAREFA_6[TAM_PILHA_6];
 uint32_t PILHA_TAREFA_7[TAM_PILHA_7];
 uint32_t PILHA_TAREFA_8[TAM_PILHA_8];
+uint32_t PILHA_TAREFA_9[TAM_PILHA_9];
 uint32_t PILHA_TAREFA_OCIOSA[TAM_PILHA_OCIOSA];
 
 /*
@@ -77,10 +80,13 @@ int main(void)
 	/* Criacao das tarefas */
 	/* Parametros: ponteiro, nome, ponteiro da pilha, tamanho da pilha, prioridade da tarefa */
 	
-	CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 1);
+	CriaTarefa(tarefa_9, "Tarefa 9", PILHA_TAREFA_9, TAM_PILHA_9, 1);
 	
-	CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 2);
+	/*CriaTarefa(tarefa_1, "Tarefa 1", PILHA_TAREFA_1, TAM_PILHA_1, 2);
 	
+	CriaTarefa(tarefa_2, "Tarefa 2", PILHA_TAREFA_2, TAM_PILHA_2, 3);*/
+	
+
 	/* Cria tarefa ociosa do sistema */
 	CriaTarefa(tarefa_ociosa,"Tarefa ociosa", PILHA_TAREFA_OCIOSA, TAM_PILHA_OCIOSA, 0);
 	
@@ -243,5 +249,66 @@ void tarefa_8(void)
 		(void)valor;	/* leitura da variável para evitar aviso (warning) do compilador */
 		
 		SemaforoLibera(&SemaforoVazio);
+	}
+}
+
+void tarefa_9(void) /*Cria um sinal de Socorro "SOS" com o led do uC */
+{
+	volatile uint16_t a = 0;
+	for(;;)
+	{
+		a++;	
+			
+				/* Liga LED. */
+		port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE);
+		TarefaEspera(500); 	/* tarefa 1 se coloca em espera por 3 marcas de tempo (ticks) */
+		
+		/* Desliga LED. */
+		port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE);
+		TarefaEspera(500); 	/* tarefa 1 se coloca em espera por 3 marcas de tempo (ticks) */
+		
+				/* Liga LED. */
+		port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE);
+		TarefaEspera(500); 	/* tarefa 1 se coloca em espera por 3 marcas de tempo (ticks) */
+		
+		/* Desliga LED. */
+		port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE);
+		TarefaEspera(500); 	/* tarefa 1 se coloca em espera por 3 marcas de tempo (ticks) */
+		
+				/* Liga LED. */
+		port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE);
+		TarefaEspera(500); 	/* tarefa 1 se coloca em espera por 3 marcas de tempo (ticks) */
+		
+		/* Desliga LED. */
+		port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE);
+		TarefaEspera(500); 	/* tarefa 1 se coloca em espera por 3 marcas de tempo (ticks) */
+		
+		
+		
+		
+		/* Liga LED. */
+		port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE);
+		TarefaEspera(1500); 	/* tarefa 1 se coloca em espera por 3 marcas de tempo (ticks) */
+		
+		/* Desliga LED. */
+		port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE);
+		TarefaEspera(1500); 	/* tarefa 1 se coloca em espera por 3 marcas de tempo (ticks) */
+		
+				/* Liga LED. */
+		port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE);
+		TarefaEspera(1500); 	/* tarefa 1 se coloca em espera por 3 marcas de tempo (ticks) */
+		
+		/* Desliga LED. */
+		port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE);
+		TarefaEspera(1500); 	/* tarefa 1 se coloca em espera por 3 marcas de tempo (ticks) */
+		
+				/* Liga LED. */
+		port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE);
+		TarefaEspera(1500); 	/* tarefa 1 se coloca em espera por 3 marcas de tempo (ticks) */
+		
+		/* Desliga LED. */
+		port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE);
+		TarefaEspera(1500); 	/* tarefa 1 se coloca em espera por 3 marcas de tempo (ticks) */
+		
 	}
 }
